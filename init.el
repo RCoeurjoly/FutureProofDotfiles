b5d3dd68affd15909b35364b7e3b2ce299e089cc
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2024-04-18 21:33:37 +0200

    Add readme

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
-(require 'org)
-(org-babel-load-file
- (expand-file-name "configuration.org"))
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (require 'org)
+            (org-babel-load-file
+             (expand-file-name "configuration.org"))))
